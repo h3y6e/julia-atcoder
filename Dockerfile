188aef6382561cc6630658cf66de29f8b1cb5f2e
5ebec <hash.a5ebec@gmail.com>
2020-06-15 07:51:42 +0900

    add function for downloading sample cases (no login)

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,10 @@ RUN julia -e 'using Pkg;\
               Pkg.add(["Atom", "Juno"]);\
               using Atom, Juno'
 
+RUN julia -e 'using Pkg;\
+              Pkg.add(["HTTP", "Gumbo", "Cascadia"]);\
+              using HTTP, Gumbo, Cascadia'
+
 COPY startup.jl /root/.julia/config/
 
 WORKDIR /contests
--- startup.jl
+++ startup.jl
@@ -1,24 +1,77 @@
 using Atom, Juno
+using Cascadia, Gumbo, HTTP
 
-function connect_juno()
-  print("ATOM_PORT: ")
-  atom_port = parse(Int, readline())
-  Juno.connect("host.docker.internal", atom_port)
+function connectjuno()
+    print("ATOM_PORT: ")
+    atomport = parse(Int, readline())
+    Juno.connect("host.docker.internal", atomport)
 end
 
-function move_contest_dir()
-  print("Contest: ")
-  contest_name = readline()
-  try
-    mkdir(contest_name)
-  catch
-  finally
+function makecontestdir()
+    print("Contest: ")
+    contestname = readline()
     try
-      cd(contest_name)
+        mkdir(contestname)
+        downloadtestcases(contestname)
     catch
+    finally
+        try
+            cd(contestname)
+        catch
+        end
     end
-  end
 end
 
-connect_juno()
-move_contest_dir()
+function downloadtestcases(contest)
+    # base
+    baseurl = "https://atcoder.jp"
+    basedir = "$(contest)/testcases"
+    mkdir(basedir)
+
+    # Get tasks
+    res = HTTP.get("$(baseurl)/contests/$(contest)/tasks")
+    tasks = eachmatch(
+        Selector(".text-center.no-break > a"),
+        parsehtml(String(res.body)).root,
+    )
+
+    for task in tasks
+        # Create directory from task url
+        href = task.attributes["href"]
+        taskdir = "$(basedir)/$(href[end])"
+        mkdir(taskdir)
+
+        # Get samples
+        r = HTTP.get("$(baseurl)$(href)")
+        h = parsehtml(String(r.body))
+        sampleinputs =
+            (eachmatch(sel"h3:contains('Sample Input') + pre", h.root))
+        sampleoutputs =
+            (eachmatch(sel"h3:contains('Sample Output') + pre", h.root))
+
+        # Create sample input files
+        mkdir("$(taskdir)/in")
+        for (i, sampleinput) in enumerate(nodeText.(sampleinputs))
+            open(
+                "$(taskdir)/in/sample_$(lpad(string(i), 2, '0')).txt",
+                "w",
+            ) do f
+                print(f, sampleinput)
+            end
+        end
+
+        # Create sample output files
+        mkdir("$(taskdir)/out")
+        for (i, sampleoutput) in enumerate(nodeText.(sampleoutputs))
+            open(
+                "$(taskdir)/out/sample_$(lpad(string(i), 2, '0')).txt",
+                "w",
+            ) do f
+                print(f, sampleoutput)
+            end
+        end
+    end
+end
+
+connectjuno()
+makecontestdir()
